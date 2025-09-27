a17855a62854764da82618b421de50b2074526bb
WAYNEisBATMAN <p.jangraaa@gmail.com>
2025-09-27 15:47:06 +0530

    vsc

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,3 @@
-output "vpc_id" {
-  value = module.network.vpc_id
-}
-
-output "public_subnets" {
-  value = module.network.public_subnets
-}
-
-
-output "db_instance" {
-  value = module.database.db_instance
-}
-
-output "s3_bucket" {
-  value = module.storage.bucket_name
-}
-
-output "sns_topic" {
-  value = module.monitoring.sns_topic
-}
-
-
 output "ec2_public_ip" {
   value = module.compute.ec2_public_ip
 }
