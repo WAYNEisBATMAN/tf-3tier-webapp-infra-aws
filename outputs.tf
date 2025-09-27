2bdb0a810f2651fc42cc0d12c2fa15625093f7a3
WAYNEisBATMAN <p.jangraaa@gmail.com>
2025-09-27 15:53:23 +0530

    vsc

--- outputs.tf
+++ outputs.tf
@@ -18,18 +18,18 @@ output "cloudwatch_alarm" {
   value = module.monitoring.alarm_name
 }
 
-output "vpc_id" {
-  value = module.network.aws_vpc.main.id
+# output "vpc_id" {
+#   value = module.network.aws_vpc.main.id
 
-}
+# }
 
-output "public_subnet_id" {
-  value = module.network.aws_subnet.public.id
-}
+# output "public_subnet_id" {
+#   value = module.network.aws_subnet.public.id
+# }
 
-output "web_sg_id" {
-  value = module.network.aws_security_group.web_sg.id
-}
+# output "web_sg_id" {
+#   value = module.network.aws_security_group.web_sg.id
+# }
 
 
 
